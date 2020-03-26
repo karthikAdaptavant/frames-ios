ed156c24e6612cc648a025ffa0568543207fadd4
Karthik samy <karthik.samy@a-cti.com>
2020-03-27 02:40:26 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:4.1
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "FramesIos",
+    platforms: [.iOS(.v10)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
